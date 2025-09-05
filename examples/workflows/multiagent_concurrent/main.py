b0801b01013689ad335325fe6ba593c058223ffc
DavdGao <gaodawei.gdw@alibaba-inc.com>
2025-09-05 16:44:34 +0800

    WIP

---
+++ examples/workflows/multiagent_concurrent/main.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""Parallel Multi-Perspective Discussion System."""
+import asyncio
+from datetime import datetime
+from typing import Any
+
+from agentscope.agent import AgentBase
+from agentscope.message import Msg
+
+
+class ExampleAgent(AgentBase):
+    """The example agent used to label the time."""
+
+    def __init__(self, name: str) -> None:
+        """The constructor of the example agent
+
+        Args:
+            name (`str`):
+                The agent name.
+        """
+        super().__init__()
+        self.name = name
+
+    async def reply(self, *args: Any, **kwargs: Any) -> Msg:
+        """The reply function of the example agent."""
+        # we record the start time
+        start_time = datetime.now().strftime("%H:%M:%S")
+
+        # Sleep 5 seconds
+        await asyncio.sleep(5)
+
+        end_time = datetime.now().strftime("%H:%M:%S")
+        msg = Msg(
+            self.name,
+            f"{self.name} begins at {start_time} " f"and finish at {end_time}",
+            "user",
+        )
+        await self.print(msg)
+        return msg
+
+    async def handle_interrupt(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Msg:
+        """We leave this function unimplemented in this example, because we
+        won't use the interrupt functionality"""
+
+    async def observe(self, *args: Any, **kwargs: Any) -> None:
+        """Similar with the handle_interrupt function, leaving this empty"""
+
+
+async def main() -> None:
+    """The main entry of the concurrent example."""
+    alice = ExampleAgent("Alice")
+    bob = ExampleAgent("Bob")
+    chalice = ExampleAgent("Chalice")
+
+    print("Use 'asyncio.gather' to run the agents concurrently:")
+    futures = [alice(), bob(), chalice()]
+
+    await asyncio.gather(*futures)
+
+    print("User fanout pipeline to run the agents concurrently:")
