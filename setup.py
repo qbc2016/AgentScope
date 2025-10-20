63a328512193da5e0035aa042fd7fe466226cd26
qbc <qianbingchen.qbc@alibaba-inc.com>
2025-10-20 10:16:23 +0800

    delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Setup script for AgentScope."""
-
-from setuptools import setup
-
-# All configuration is now in pyproject.toml
-# This file exists only for backward compatibility
-setup()
